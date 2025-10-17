fd05a27d49ab7e2ae53ef9d53987029c24b3866c
florahou0w0 <hou.chi@northeastern.edu>
2025-10-16 23:13:42 -0400

    debug

--- app/Labs/Lab3/BooLeanVariables.tsx
+++ app/Labs/Lab3/BooLeanVariables.tsx
@@ -7,7 +7,7 @@ export default function BooLeanVariables() {
     const true2 = true1 || false1;
     const true3 = !false2;
     const true4 = numberVariable === 123; // always use === not ==
-    // @ts-expect-error
+    // @ts-expect-error learning boolean
     const true5 = floatingPointNumber !== 321.432;
     const false3 = numberVariable < 100;
     return (
