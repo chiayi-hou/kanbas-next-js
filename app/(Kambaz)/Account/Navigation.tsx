ab9c99c98f49dd9bb826a6caf88bbdede50b8a8f
florahou0w0 <hou.chi@northeastern.edu>
2025-11-26 12:06:26 -0800

    a6

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -7,12 +7,15 @@ import { usePathname } from "next/navigation";
 
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const links = currentUser
+  ? ["Profile", ...(currentUser.role === "ADMIN" ? ["Users"] : [])]
+  : ["Signin", "Signup"];
   const pathname = usePathname();
  return (
    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
     {links.map((link)=>(
       <Link key={link} href={link} className={`list-group-item border-0 ${pathname.endsWith(link)? "active":"text-danger"}`}> {link} </Link>
     ))}
+    
    </div>
 );}
--- app/(Kambaz)/Courses/[cid]/People/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/page.tsx
@@ -1,12 +1,26 @@
 "use client"; 
 
 import { redirect } from "next/navigation";
+import { useState, useEffect } from "react";
 import { usePathname } from 'next/navigation';
+import PeopleTable from "./Table";
+import * as client from "../../../Account/client"
 
 export default function People() 
 {
   const pathname = usePathname();
+  const [users, setUsers] = useState<any[]>([]);
+
+  const fetchUsersForCourse = async () => {
+   const users = await client.findAllUsers();
+   setUsers(users);
+ };
+ useEffect(() => {
+   fetchUsersForCourse();
+ }, []);
+
   return (
-    redirect(`${pathname}/Table`)
+    <PeopleTable users={users} fetchUsers={fetchUsersForCourse} />
+    //redirect(`${pathname}/Table`)
   )
 }
